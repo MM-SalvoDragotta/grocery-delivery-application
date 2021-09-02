317f0ee72a877159c1b3800b76a6a5dd91a9f4db
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-09-02 21:20:07 +1000

    developing routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { Product, Category } = require('../models');
-const withAuth = require('../utils/auth');
+//const withAuth = require('../utils/auth');
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/');
     return;
   }
 
@@ -13,14 +13,14 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/');
     return;
   }
 
   res.render('signup');
 });
 
-router.get('/', async (req, res) => {
+router.get('/category', async (req, res) => {
   try {
     const cateData = await Category.findAll({
       include: [
@@ -35,12 +35,14 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-
+    
     const categories = cateData.map((cate) => cate.get({ plain: true }));
-    res.render('', { 
-      categories,
-      logged_in: req.session.logged_in, 
-    });
+
+    res.status(200).json(categories);
+    // res.render('', { 
+    //   categories,
+    //   logged_in: req.session.logged_in, 
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -64,25 +66,27 @@ router.get('/:id', async (req, res) => {
 
     const category = cateData.get({ plain: true });
 
-    res.render('', { 
-      category, 
-      logged_in: req.session.logged_in,
-    });
+    res.status(200).json(category);
+    // res.render('', { 
+    //   category, 
+    //   logged_in: req.session.logged_in,
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('product/:id', withAuth , async (req, res) => {
+router.get('/product/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id,{});
 
     const product = productData.get({ plain: true });
-
-    res.render('', { 
-      product, 
-      logged_in: req.session.logged_in,
-    });
+  
+    res.status(200).json(product);
+    // res.render('', { 
+    //   product, 
+    //   logged_in: req.session.logged_in,
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
