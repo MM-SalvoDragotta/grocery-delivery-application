d87a66cb10cdbd42a056edd29588bbf453c13f6d
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-09-07 11:38:40 +1000

    Capitalise models file name

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+//Model
 class Product extends Model {}
 
 Product.init(
