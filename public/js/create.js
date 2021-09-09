2a6398512b335d677dbe2b9800b48e4bc6df7783
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-09-09 12:40:41 +1000

    Add upload images code

--- public/js/create.js
+++ public/js/create.js
@@ -4,6 +4,10 @@ const createproduct = async (event) => {
   const price = document.querySelector('#price').value.trim();
   const stock = document.querySelector('#stock').value.trim();
   const category_id = document.querySelector('#category').value.trim();
+  const file = document.querySelector('#upload-file').files[0];
+
+  const file64 =  await toBase64(file);
+
   if(document.querySelector('#special').checked){
     isSpecial = true
   }else{
@@ -13,7 +17,7 @@ const createproduct = async (event) => {
   if (name && price && stock && category_id) {
     const response = await fetch('/api/products', {
       method: 'POST',
-      body: JSON.stringify({ name, price ,stock , category_id, isSpecial}),
+      body: JSON.stringify({ name, price ,stock , category_id, isSpecial, file64}),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -26,5 +30,12 @@ const createproduct = async (event) => {
     }
   }
 };
+
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+});
 
 document.querySelector('.create-form').addEventListener('submit', createproduct)
