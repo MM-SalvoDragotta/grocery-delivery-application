3253e7876c9e1794b6bc9ac9e08996a90931d5f6
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-09-09 21:27:32 +1000

    delete models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,6 @@
 const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
-//const Order = require('./order');
-//const ProductOrder = require('./productorder');
-
-
-// Order.belongsTo(User,{
-//   foreignKey: 'user_id'
-// });
 
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
@@ -18,24 +11,8 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Product.belongsToMany(Order, {
-//   through: {
-//     model: ProductOrder,
-//     unique: false
-//   },
-// });
-
-// Order.belongsToMany(Product, {
-//   through: {
-//     model: ProductOrder,
-//     unique: false
-//   },
-// });
-
 module.exports = {
   Product,
   Category,
- // Order,
   User,
-  //ProductOrder
 };
